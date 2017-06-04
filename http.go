18f06cf8f6c4ff142f4a305c0b0ee4babde8c088
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-03 22:24:47 -0700

    add RequestGetIPAddress()

---
+++ http.go
@@ -0,0 +1,37 @@
+package u
+
+import (
+	"net/http"
+	"strings"
+)
+
+// Request.RemoteAddress contains port, which we want to remove i.e.:
+// "[::1]:58292" => "[::1]"
+func ipAddrFromRemoteAddr(s string) string {
+	idx := strings.LastIndex(s, ":")
+	if idx == -1 {
+		return s
+	}
+	return s[:idx]
+}
+
+// RequestGetIPAddress returns ip address of the client making the request,
+// taking into account http proxies
+func RequestGetIPAddress(r *http.Request) string {
+	hdr := r.Header
+	hdrRealIP := hdr.Get("X-Real-Ip")
+	hdrForwardedFor := hdr.Get("X-Forwarded-For")
+	if hdrRealIP == "" && hdrForwardedFor == "" {
+		return ipAddrFromRemoteAddr(r.RemoteAddr)
+	}
+	if hdrForwardedFor != "" {
+		// X-Forwarded-For is potentially a list of addresses separated with ","
+		parts := strings.Split(hdrForwardedFor, ",")
+		for i, p := range parts {
+			parts[i] = strings.TrimSpace(p)
+		}
+		// TODO: should return first non-local address
+		return parts[0]
+	}
+	return hdrRealIP
+}
