672f1d477509cc730e44ba1bdc2c03c6477b6001
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2017-06-09 20:24:07 -0700

    write optimized RemoveDuplicateStrings

--- string.go
+++ string.go
@@ -1,5 +1,7 @@
 package u
 
+import "sort"
+
 // StringArrayRemoveFirst removes first
 func StringArrayRemoveFirst(a []string) []string {
 	n := len(a)
@@ -8,4 +10,24 @@ func StringArrayRemoveFirst(a []string) []string {
 		a = a[:n-1]
 	}
 	return a
+}
+
+// RemoveDuplicateStrings removes duplicate strings from an array of strings.
+// It's optimized for the case of no duplicates. It modifes a in place.
+func RemoveDuplicateStrings(a []string) []string {
+	if len(a) < 2 {
+		return a
+	}
+	sort.Strings(a)
+	writeIdx := 1
+	for i := 1; i < len(a); i++ {
+		if a[i-1] == a[i] {
+			continue
+		}
+		if writeIdx != i {
+			a[writeIdx] = a[i]
+		}
+		writeIdx++
+	}
+	return a[:writeIdx]
 }
--- string_test.go
+++ string_test.go
@@ -19,3 +19,23 @@ func TestStringArrayRemoveFirst(t *testing.T) {
 		assert.Equal(t, exp, got)
 	}
 }
+
+func TestRemoveDuplicateStrings(t *testing.T) {
+	// note: the fact that arrays are sorted after RemoveDuplicateString
+	// is accidental. We could make the tests more robust by writing
+	// doStringArraysHaveTheSameContent(a1, a2 []string)
+	tests := [][]string{
+		nil, nil,
+		[]string{"a"}, []string{"a"},
+		[]string{"b", "a"}, []string{"a", "b"},
+		[]string{"a", "a"}, []string{"a"},
+		[]string{"a", "b", "a"}, []string{"a", "b"},
+		[]string{"ab", "ba", "ab", "ab", "cd"}, []string{"ab", "ba", "cd"},
+	}
+	n := len(tests) / 2
+	for i := 0; i < n; i++ {
+		got := RemoveDuplicateStrings(tests[i*2])
+		exp := tests[i*2+1]
+		assert.Equal(t, exp, got)
+	}
+}
