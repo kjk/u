0c699071a9b1f6d7c26f385a577d723237e1ede0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-16 21:54:55 -0700

    add WaitForCtrlC()

--- util.go
+++ util.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
 	"runtime"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -191,4 +193,11 @@ func OpenBrowser(url string) error {
 		err = fmt.Errorf("unsupported platform")
 	}
 	return err
+}
+
+// WaitForCtrlC waits until a user presses Ctrl-C
+func WaitForCtrlC() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt /* SIGINT */, syscall.SIGTERM)
+	<-c
 }
