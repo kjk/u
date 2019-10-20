7a8dc715954d058c4ae41fb51d916679af5134f5
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-10-19 17:34:52 -0700

    add FormattedDuration and FormatDuration

---
+++ duration.go
@@ -0,0 +1,42 @@
+package u
+
+import (
+	"strings"
+	"time"
+)
+
+// time.Duration with a better string representation
+type FormattedDuration time.Duration
+
+func (d FormattedDuration) String() string {
+	return FormatDuration(time.Duration(d))
+}
+
+// FormatDuration formats duration in a more human friendly way
+// than time.Duration.String()
+func FormatDuration(d time.Duration) string {
+	s := d.String()
+	if strings.HasSuffix(s, "µs") {
+		// for µs we don't want fractions
+		parts := strings.Split(s, ".")
+		if len(parts) > 1 {
+			return parts[0] + " µs"
+		}
+		return strings.ReplaceAll(s, "µs", " µs")
+	} else if strings.HasSuffix(s, "ms") {
+		// for ms we only want 2 digit fractions
+		parts := strings.Split(s, ".")
+		//fmt.Printf("fmtDur: '%s' => %#v\n", s, parts)
+		if len(parts) > 1 {
+			s2 := parts[1]
+			if len(s2) > 4 {
+				// 2 for "ms" and 2+ for fraction
+				res := parts[0] + "." + s2[:2] + " ms"
+				//fmt.Printf("fmtDur: s2: '%s', res: '%s'\n", s2, res)
+				return res
+			}
+		}
+		return strings.ReplaceAll(s, "ms", " ms")
+	}
+	return s
+}
