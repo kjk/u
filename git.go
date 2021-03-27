13ea3391899193ff5d4e0ba4ca828a05f8e61db8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-03-26 23:05:56 -0700

    update IsGitClean() for 'master' => 'main' rename in GitHub

--- git.go
+++ git.go
@@ -24,18 +24,41 @@ func GitStatusMust(dir string) string {
 
 func IsGitClean(dir string) bool {
 	s := GitStatusMust(dir)
-	expected := []string{
+	expected1 := []string{
 		"On branch master",
 		"Your branch is up to date with 'origin/master'.",
 		"nothing to commit, working tree clean",
 	}
-	for _, exp := range expected {
-		if !strings.Contains(s, exp) {
-			Logf("Git repo in '%s' not clean.\nDidn't find '%s' in output of git status:\n%s\n", dir, exp, s)
-			return false
+	expected2 := []string{
+		"On branch main",
+		"Your branch is up to date with 'origin/main'.",
+		"nothing to commit, working tree clean",
+	}
+	{
+		hasAll := true
+		for _, exp := range expected1 {
+			if !strings.Contains(s, exp) {
+				//Logf("Git repo in '%s' not clean.\nDidn't find '%s' in output of git status:\n%s\n", dir, exp, s)
+				hasAll = false
+			}
+		}
+		if hasAll {
+			return true
 		}
 	}
-	return true
+	{
+		hasAll := true
+		for _, exp := range expected2 {
+			if !strings.Contains(s, exp) {
+				hasAll = false
+			}
+		}
+		if hasAll {
+			return true
+		}
+	}
+	Logf("Git repo in '%s' not clean.\nGit status:\n%s\n", dir, s)
+	return false
 }
 
 func EnsureGitClean(dir string) {
